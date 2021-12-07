365801e0a906e8209390cb33c76dd0cf7d24fcab
legolai <nico360f@gmail.com>
2021-12-07 12:36:09 +0100

    first endpoint (working) and a bit frontend

--- webapp/main.js
+++ webapp/main.js
@@ -1,11 +1,7 @@
 const baseURL = "http://localhost:8888/api";
-const form = document.querySelector("form");
-form.addEventListener("submit", (event) => {
-	// submit event detected
+
+$("#login").submit((event) => {
 	event.preventDefault();
-	// const request = new XMLHttpRequest();
-	// request.open("POST", baseURL + "/login");
-	console.log(event.username);
-	// const data = '${e}'
-	// request.send('{"username": ${e} }')
+	const data = $("#login :input").serializeArray();
+	$.ajax(baseURL+"/login").method("POST")
 });
--- webapps/api/WEB-INF/classes/LoginServlet.java
+++ webapps/api/WEB-INF/classes/LoginServlet.java
@@ -6,27 +6,44 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        User
-        if () {
-            response.setStatus(200);
-            response.addHeader("Accept", "*");
-            response.setContentType("application/json");
-            response.setCharacterEncoding("utf-8");
-            PrintWriter out = response.getWriter();
-            out.println("{\"access_token\":\"21313esadf\"}");
-            out.close();  // Always close the output writer
+        Map<String, String[]> data = request.getParameterMap();
+        response.setStatus(200);
+        response.addHeader("Accept", "*");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("{");
+        for (String key: data.keySet()) {
+            for (String s: data.get(key)) {
+                out.print("{\"access_token\":\"21313esadf\"}");
+            }
         }
-        else {
-            response.setStatus(401);
-        }
+        out.println("}");
+        out.close();  // Always close the output writer
+
+
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        if (username.equalsIgnoreCase("admin") && password.equals("Admin")) {
+//            response.setStatus(200);
+//            response.addHeader("Accept", "*");
+//            response.setContentType("application/json");
+//            response.setCharacterEncoding("utf-8");
+//            PrintWriter out = response.getWriter();
+//            out.println("{\"access_token\":\"21313esadf\"}");
+//            out.close();  // Always close the output writer
+//        }
+//        else {
+//            response.setStatus(401);
+//        }
     }
 
 }
