e6f12ee257d9f0d8282545d07b618e71e8fb336d
legolai <nico360f@gmail.com>
2021-12-08 13:35:27 +0100

    colorplatte add jaja

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -15,29 +15,9 @@ const doPost = (endpoint, data) => {
         type: "POST",
         data: JSON.stringify(data)  ,
         success: (result, status, xhr) => {
-            window.location.replace("login.html");
+            window.location.replace("index.html");
         }
     })
 }
 
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
 
