9933eb160f47fb8c700b6b2d0dba210d744c2d50
legolai <nico360f@gmail.com>
2021-12-08 11:14:20 +0100

    first endpoint (working) with json

--- webapp/main.js
+++ webapp/main.js
@@ -2,6 +2,8 @@ const baseURL = "http://localhost:8888/api";
 
 $("#login").submit((event) => {
 	event.preventDefault();
-	const data = $("#login :input").serializeArray();
-	$.ajax(baseURL+"/login").method("POST")
+
+	const data = $("#login :input").serializeArray().map((item) => {return [item.name, item.value];});
+	console.log(event.target);
+	console.log(data);
 });
